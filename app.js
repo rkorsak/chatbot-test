ef6066e9aac89a52ed6ca6ab7e6201ba894adeae
rkorsak <rkorsak@gmail.com>
2016-03-30 15:40:19 -0700

    Change casing of port env var

--- app.js
+++ app.js
@@ -15,6 +15,6 @@ bot.add('/', function (session) {
 // Setup Restify Server
 var server = restify.createServer()
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen())
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.PORT || 3978, function () {
   console.log('%s listening to %s', server.name, server.url)
 })
